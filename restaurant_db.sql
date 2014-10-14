9a7fd84a9a8550608ba61768cee8dc3b0e45c3b1
JamesCPLiu <jamesc.pliu@gmail.com>
2014-10-14 12:22:29 -0400

    Restaurant Database Set Up

---
+++ restaurant_db.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS parties CASCADE;
+DROP TABLE IF EXISTS foods CASCADE;
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE parties(
+  id SERIAL PRIMARY KEY,
+  table integer,
+  groupsize integer,
+  paid_check boolean
+);
+
+CREATE TABLE foods(
+  id SERIAL PRIMARY KEY,
+  dishname varchar(255),
+  price money,
+  allergens varchar(510),
+  calories integer
+);
+
+CREATE TABLE orders(
+  id SERIAL PRIMARY KEY,
+  time TIMESTAMP,
+  party_id INTEGER,
+  food_id INTEGER
+);
